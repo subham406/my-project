9b757eb0c254d60803bcf387154bb9bf22eb8139
mandeep kumar mandal <subhammandal0123457@gmail.com>
2026-02-22 23:00:07 -0800

    text

---
+++ pythonniis/basicpython/length.py
@@ -0,0 +1,10 @@
+print("enter square length")
+s=int(input())
+print("enter square breadth")
+b=int(input())
+ar=s*s
+pr=4*s
+print("length=",s)
+print("breadth=",b)
+print("area=",ar)
+print("perimeter=",pr)
